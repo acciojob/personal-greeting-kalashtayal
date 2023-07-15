b143ef46ab536f6d570de75ca42f91c712714fcd
tayal <kalashtayal2000@gmail.com>
2023-07-15 15:02:09 +0530

    submitting code-Sat Jul 15 2023 15:02:07 GMT+0530 (India Standard Time)

---
+++ src/components/Greeting.js
@@ -0,0 +1,20 @@
+import React , {useState} from "react";
+
+function Greeting(){
+    const [name,setName]= useState('');
+    const [greeting,setGreeting] = useState('');
+
+    const handleChange= (event)=>{
+        setName( event.target.value);
+    };
+    return(
+        <div>
+            <label>Enter your name :</label><br/>
+           
+ <input type="text" value={name} onChange={handleChange}/>
+
+ <p>{"Hello" + name+ "!"}</p>
+        </div>
+    );
+}
+export default Greeting;
